4bf790bc0c2fbc2034b01ee883e4086cb9bb2cd3
ludwig.ettner@web.de <ludwig.ettner@web.de>
2024-03-05 14:01:45 +0100

    Add new files and make changes to existing files

---
+++ 8_Recap/Schleifen.py
@@ -0,0 +1,24 @@
+def weihnachtsbaum(hoehe: int) -> None:
+    """
+    Prints a Christmas tree of the given height.
+
+    Parameters:
+    - hoehe (int): The height of the Christmas tree.
+
+    Returns:
+    - None
+    """
+    raise NotImplementedError("Implementiere die Funktion")
+
+######################
+
+arbeitsstunden_pro_woche = {
+    'Montag': 8,
+    'Dienstag': 7,
+    'Mittwoch': 8,
+    'Donnerstag': 6,
+    'Freitag': 9,
+    'Samstag': 5,
+    'Sonntag': 0  # Der Schüler hat am Sonntag nicht gearbeitet
+}
+
---
+++ 8_Recap/Temperaturen.py
@@ -0,0 +1,128 @@
+def celsius_to_fahrenheit(celsius: float) -> float:
+    """
+    Converts temperature from Celsius to Fahrenheit.
+
+    Args:
+        celsius (float): Temperature in Celsius.
+
+    Returns:
+        float: Temperature in Fahrenheit.
+
+    Examples:
+        >>> celsius_to_fahrenheit(0)
+        32.0
+        >>> celsius_to_fahrenheit(100)
+        212.0
+        >>> celsius_to_fahrenheit(-40)
+        -40.0
+    """
+    raise NotImplementedError("Please implement this function")
+
+
+def celsius_to_kelvin(celsius: float) -> float:
+    """
+    Converts temperature from Celsius to Kelvin.
+
+    Args:
+        celsius (float): Temperature in Celsius.
+
+    Returns:
+        float: Temperature in Kelvin.
+
+    Raises:
+        NotImplementedError: If the function is not implemented.
+
+    Examples:
+        >>> celsius_to_kelvin(0)
+        273.15
+        >>> celsius_to_kelvin(25)
+        298.15
+    """
+    raise NotImplementedError("Please implement this function")
+
+
+def fahrenheit_to_celsius(fahrenheit: float) -> float:
+    """
+    Converts a temperature from Fahrenheit to Celsius.
+
+    Args:
+        fahrenheit (float): The temperature in Fahrenheit.
+
+    Returns:
+        float: The temperature in Celsius.
+
+    Examples:
+        >>> fahrenheit_to_celsius(32)
+        0.0
+        >>> fahrenheit_to_celsius(212)
+        100.0
+        >>> fahrenheit_to_celsius(98.6)
+        37.0
+    """
+    raise NotImplementedError("Please implement this function")
+
+
+def fahrenheit_to_kelvin(fahrenheit: float) -> float:
+    """
+    Converts temperature from Fahrenheit to Kelvin.
+
+    Args:
+        fahrenheit (float): Temperature in Fahrenheit.
+
+    Returns:
+        float: Temperature in Kelvin.
+
+    Raises:
+        NotImplementedError: If the function is not implemented.
+
+    Examples:
+        >>> fahrenheit_to_kelvin(32)
+        273.15
+        >>> fahrenheit_to_kelvin(212)
+        373.15
+    """
+    raise NotImplementedError("Please implement this function")
+
+
+def kelvin_to_celsius(kelvin: float) -> float:
+    """
+    Converts temperature from Kelvin to Celsius.
+
+    Args:
+        kelvin (float): Temperature in Kelvin.
+
+    Returns:
+        float: Temperature in Celsius.
+
+    Raises:
+        NotImplementedError: If the function is not implemented.
+
+    Examples:
+        >>> kelvin_to_celsius(273.15)
+        0.0
+        >>> kelvin_to_celsius(0)
+        -273.15
+    """
+    raise NotImplementedError("Please implement this function")
+
+
+def kelvin_to_fahrenheit(kelvin: float) -> float:
+    """
+    Converts temperature in Kelvin to Fahrenheit.
+
+    Args:
+        kelvin (float): Temperature in Kelvin.
+
+    Returns:
+        float: Temperature in Fahrenheit.
+
+    Raises:
+        NotImplementedError: If the function is not implemented.
+
+    Examples:
+        >>> kelvin_to_fahrenheit(273.15)
+        32.0
+        >>> kelvin_to_fahrenheit(0)
+        -459.67
+    """
+    raise NotImplementedError("Please implement this function")
