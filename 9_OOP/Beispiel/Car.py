0d73f9238485fe88171a3c70c0a6346e5970d973
ludwig.ettner@web.de <ludwig.ettner@web.de>
2024-04-18 14:17:33 +0200

    Add Car class and test script for car functionality

---
+++ 9_OOP/Beispiel/Car.py
@@ -0,0 +1,18 @@
+class car:
+    def __init__(self, pos_x: int, pos_y: int, direction: str, is_alive: bool):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.direction = direction
+        self.is_alive = is_alive
+    
+    def drive(self, pos_x: int, pos_y: int, direction: str):
+        if self.is_alive:
+            self.pos_x = pos_x
+            self.pos_y = pos_y
+            self.direction = direction
+    
+    def honk(self):
+        print("mep mep")
+    
+    def explode(self):
+        self.is_alive = False
---
+++ 9_OOP/Beispiel/test.py
@@ -0,0 +1,12 @@
+from Car import *
+
+stuck = car(0, 0, "none", True)
+
+print(stuck.is_alive)
+
+print(stuck.pos_x, stuck.pos_y)
+
+stuck.drive(20, 20, "north")
+
+print(stuck.pos_x, stuck.pos_y)
+
